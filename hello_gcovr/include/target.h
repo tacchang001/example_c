82a62a43bd003520b7863dc977b0efebce203051
tacchangxyz <user001@tacchang.xyz>
2018-12-08 23:10:54 +0900

    gcovrサンプルのベースを作成した。これからgcovrを盛り込んでいく。

---
+++ hello_gcovr/include/target.h
@@ -0,0 +1,18 @@
+//
+// Created by devel on 18/12/01.
+//
+
+#ifndef TARGET_H
+#define TARGET_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+extern int target_function(const int a, const int b);
+
+#ifdef __cplusplus
+}
+#endif  // __cplusplus
+
+#endif //TARGET_H
---
+++ hello_gcovr/tests/test_target.cpp
@@ -0,0 +1,26 @@
+#include "CppUTest/CommandLineTestRunner.h"
+#include "target.h"
+
+// テストグループを定義
+TEST_GROUP(TestFuncGroup) {
+    // 各テストケースの実行直前に呼ばれる仮想メソッド
+    TEST_SETUP() {
+
+    }
+
+    // 各テストケースの実行直後に呼ばれる仮想メソッド
+    TEST_TEARDOWN() {
+
+    }
+};
+
+// テストを実行するメソッド
+TEST(TestFuncGroup, Test1)
+{
+    int ans = target_function(1, 2);
+    CHECK_EQUAL(3, ans);
+}
+
+int main(int argc, char **argv) {
+    return RUN_ALL_TESTS(argc, argv);
+}
